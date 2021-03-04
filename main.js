c4bcb7211e2857adf2b490f34c74f50ead06cc58
slobodanmiric08051977 <slobamiric@gmail.com>
2021-03-04 18:39:43 +0100

    kraj proslog casa

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //document.querySelectorAll(".navButton");
 
-Array.from(querySelectorAll(".navButton")).forEach(item => {
+Array.from(document.querySelectorAll(".navButton")).forEach(item => {
     item.onclick = () => {
         item.parentElement.parentElement.classList.toggle('cardChange');
     }
