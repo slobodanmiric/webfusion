b9414687d03e611d332aa2a1d8c54f6a37533766
slobodanmiric08051977 <slobamiric@gmail.com>
2021-03-04 19:14:19 +0100

    kuckanje funkcija

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ Array.from(document.querySelectorAll(".navButton")).forEach(item => {
         item.parentElement.parentElement.classList.toggle('cardChange');
     }
 });
+
+const naslov = "Web design and development";
+let i = 0;
+const kuckanje = () => {
+    if(i < naslov.length) {
+        document.querySelector('.tipkajuciNaslov').innerHTML += naslov.charAt(i);
+        i++;
+        setTimeout(kuckanje, 100);
+    }
+}
+kuckanje();
