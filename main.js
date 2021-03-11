ae3dc43734598143438a27ff1599addf54ad17f8
slobodanmiric08051977 <slobamiric@gmail.com>
2021-03-11 19:51:12 +0100

    finale

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+var jsView = document.querySelector('#jsView');
+var phpView = document.querySelector('#phpView');
+var wpView = document.querySelector('#wpView');
+var jsViewImg = document.querySelector('#jsViewImg');
+var phpViewImg = document.querySelector('#phpViewImg');
+var wpViewImg = document.querySelector('#wpViewImg');
+
+function showJsView() {
+    jsView.style.display = "block";
+    phpView.style.display = "none";
+    wpView.style.display = "none";
+    jsViewImg.style.display = "block";
+    phpViewImg.style.display = "none";
+    wpViewImg.style.display = "none";
+}
+
+function showPhpView() {
+    jsView.style.display = "none";
+    phpView.style.display = "block";
+    wpView.style.display = "none";
+    jsViewImg.style.display = "none";
+    phpViewImg.style.display = "block";
+    wpViewImg.style.display = "none";
+}
+
+function showWpView() {
+    jsView.style.display = "none";
+    phpView.style.display = "none";
+    wpView.style.display = "block";
+    jsViewImg.style.display = "none";
+    phpViewImg.style.display = "none";
+    wpViewImg.style.display = "block";
+}
+
 //   cards
 Array.from(document.querySelectorAll(".navButton")).forEach(item => {
     item.onclick = () => {
@@ -28,12 +62,5 @@ var loop = setInterval(function() {
     }
     img.setAttribute('src', 'img/img' + brojac + '.jpg');
 }, 4000);
-
-
-
-
-
-
-
 
 
